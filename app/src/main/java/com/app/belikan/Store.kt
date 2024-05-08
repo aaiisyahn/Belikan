1ed96b4ac21dd95c1daf54e9e7228503b13c9e2e
aaiisyahn <you@example.com>
2024-05-08 10:58:09 +0700

    commint main activity full

--- app/src/main/java/com/app/belikan/Store.kt
+++ app/src/main/java/com/app/belikan/Store.kt
@@ -6,7 +6,7 @@ data class Store(val imageResId: Int, val text1: String, val text2: String) {
     companion object {
         val dataList = arrayListOf(
             Store(R.drawable.toko1, "Toko Ikan Laku", "Tanjung Priuk, Jakarta Utara"),
-            Store(R.drawable.toko1, "Text 1-1", "Text 1-2"),
+            Store(R.drawable.toko2, "Rumah Ikan", "Cilincing, Jakarta Utara"),
             Store(R.drawable.toko1, "Text 1-1", "Text 1-2"),
 
             Store(R.drawable.toko1, "Text 1-1", "Text 1-2"),
